f4f9d196d3e0bed81df35188072df67069acfbb4
Njeri Kieha <sylvia.kieha@andela.com>
2016-08-02 12:44:44 +0300

    sequelize and postgres setup

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
+/* eslint-disable no-console */
+
 // get dependencies
 var app = require('express')();
+var Sequelize = require('sequelize');
+
+// sequelize initialization
+var sequelize = new Sequelize('postgres://njerikieha:njerikieha@localhost:5432/passwordtracker');
+
+// check db connection
+sequelize
+.authenticate()
+.then(function() {
+	console.log('Connection has been established successfully.');
+})
+.catch(function(err) {
+	console.log('Unable to connect to database:', err);
+});
 
 // handle request and response
 app.get('/', function(req, res) {
