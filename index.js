50bde94ef86c8352e24adf0f45128c594c2bc38b
Njeri Kieha <sylvia.kieha@andela.com>
2016-08-02 15:36:07 +0300

    model definition

--- index.js
+++ index.js
@@ -17,6 +17,30 @@ sequelize
 	console.log('Unable to connect to database:', err);
 });
 
+// model definition
+var User = sequelize.define('user', {
+	username: {
+		type: Sequelize.STRING
+	},
+	password: {
+		type: Sequelize.STRING
+	}
+});
+
+// sync model with db, force: true drops the db if it already exists
+sequelize.sync({force: true}).then(function() {
+	return User.create({
+		username: 'njeri',
+		password: 'secret'
+	}).then(function() {
+		User.find({
+			where: {username: 'njeri'}
+		}).then(function(njeri) {
+			console.log('Hello ' + njeri.username);
+		});
+	});
+});
+
 // handle request and response
 app.get('/', function(req, res) {
 	res.send({name: 'Hello World'});
